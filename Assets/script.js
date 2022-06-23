e2496ae187a76600a3a35193cbc0fb0880613871
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-06-23 10:57:37 +0300

    Makes the page fully responsive

--- Assets/script.js
+++ Assets/script.js
@@ -71,7 +71,7 @@ function getWeather(latitude, longitude){
 
 // DISPLAY WEATHER TO UI
 function displayWeather(){
-    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+    iconElement.innerHTML = `<img src="./Images/icons${weather.iconId}.png"/>`;
     tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
     descElement.innerHTML = weather.description;
     locationElement.innerHTML = `${weather.city}, ${weather.country}`;
